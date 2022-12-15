5dac108aabae137384d51b65e42abad5a9033a7f
duc-cute <ducnguyen150120035555@gmail.com>
2022-12-15 12:18:52 +0700

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,46 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = $(".nav__menu");
+const navToggle = $(".nav__toggle");
+const navClose = $(".nav__close");
+const navItem = $$(".nav__item");
 
+const hideMenu = () => {
+  navMenu.classList.remove("show-menu");
+};
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+const showMenu = () => {
+  navMenu.classList.add("show-menu");
+};
 
+if (navToggle) {
+  navToggle.addEventListener("click", showMenu);
+}
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", hideMenu);
+}
 
-
+navItem.forEach((element) => {
+  element.addEventListener("click", hideMenu);
+});
 /*==================== REMOVE MENU MOBILE ====================*/
-
 
 /*==================== ACCORDION SKILLS ====================*/
 
-
 /*==================== QUALIFICATION TABS ====================*/
-
 
 /*==================== SERVICES MODAL ====================*/
 
-
 /*==================== PORTFOLIO SWIPER  ====================*/
-
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
